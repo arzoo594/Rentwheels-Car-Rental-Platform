9f08bdf4f8d3b906355612a8ea03ec96c67a0038
arzoo594 <arzooahmed0170609@gmail.com>
2025-11-08 23:02:48 +0600

    delete api created in my projects

--- index.js
+++ index.js
@@ -60,6 +60,15 @@ async function run() {
       res.send(result);
     });
 
+    //  Delete Api
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await carsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
