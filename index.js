f79e3cd38cb3569f60cae3a0a3fd70fc0fadc4d2
arzoo594 <arzooahmed0170609@gmail.com>
2025-11-11 23:46:34 +0600

    Environment Variable Created In server side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://Car-Rental-Platform:zjGpeabtx0NGGJwq@cluster0.9a0hyyx.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9a0hyyx.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
